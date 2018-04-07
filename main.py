bf181d899b9189a9755815f2a97beb57b6d8899c
David Clevenger <clevengeriv@gmail.com>
2018-04-07 18:51:23 -0400

    Added Pixel class. Cleaned up output file.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from sklearn.cluster import KMeans
 
+class Pixel:
+    def set(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+
 class Image:
     def read_img(self, path):
         f = open(path)
@@ -8,42 +14,31 @@ class Image:
         f.readline()
         self.r, self.c = f.readline().split(' ') #row, col
         self.max = f.readline() #max value
-        self.data = f.readline() #img data
+        self.max = int(self.max)
+        self.raw = f.readline() #raw image data
 
         self.pix = []
-        for i in range(0,len(self.data)-2,3):
-            self.pix.append(list(self.data[i:i+3]))
+        for i in range(0,len(self.raw)-2,3):
+            p = Pixel()
+            Pixel.set(p,ord(self.raw[i]), ord(self.raw[i+1]), ord(self.raw[i+2]))
+            self.pix.append(p)
 
-        self.data = [] # reset type
-        for i in range(0,len(self.pix)):
-            l = []
-            for j in range(0,len(self.pix[i])):
-                l.append(ord(self.pix[i][j]))
-            self.data.append(l) #append converted values
 
-    def write_img(self, path, m, pix):
+    def write_img(self, path):
         f = open(path, 'wr')
         f.write('P6\n')
-        f.write('120') #new size
+        f.write(self.r)
         f.write(' ')
-        f.write('120')
+        f.write(self.c)
         f.write('\n')
-        f.write(str(m)) #max pixel value
+        f.write(str(self.max))
         f.write('\n')
 
-        #r zfill(3)
-        #g zfill(3)
-        #b zfill(2)
-        for i in range(0, len(pix), 3):
-            for j in range(0,3):
-                if j == 0: #red
-                    f.write(bin(pix[i+j])[2:].zfill(3))
-                if j == 1: #green
-                    f.write(bin(pix[i + j])[2:].zfill(3))
-                if j == 2: #blue
-                    f.write(bin(pix[i + j])[2:].zfill(2))
-            #print bin(pix[i+j]).zfill(8)
-            #f.write(bin(pix[i]))
+        for i in range(0, len(self.pix)):
+            f.write(chr(self.pix[i].r))
+            f.write(chr(self.pix[i].g))
+            f.write(chr(self.pix[i].b))
+
         f.close()
 
 
@@ -51,11 +46,11 @@ class Image:
 
 img = Image()
 img.read_img('/Users/davidclevenger/PycharmProjects/Pattern-Recognition/flowers.ppm')
-print img.t, img.max
+#print img.t, img.max
 
-model = KMeans(8,n_jobs=-1)
-y = model.fit(img.data)
-print y.labels_
-img.write_img('/Users/davidclevenger/PycharmProjects/Pattern-Recognition/8bit.ppm',3,y.labels_)
+#model = KMeans(8,n_jobs=-1)
+#y = model.fit_transform(img.data)
+#print y
+img.write_img('/Users/davidclevenger/PycharmProjects/Pattern-Recognition/flowers-comp.ppm')
 
 
